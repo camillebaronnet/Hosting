5f7cb164b0cfef30c082d71b186b8a4f3b16887f
Camille Baronnet <git@camillebaronnet.fr>
2015-05-20 13:48:55 +0200

    Modification des permissions

--- startup.sh
+++ startup.sh
@@ -3,12 +3,15 @@
 # Check directory
 if [ ! -d "/home/conf/" ]; then
 	mkdir -p /home/conf/
+	chmod -R 777 /home/conf/
 fi
 if [ ! -d "/home/logs/" ]; then
 	mkdir -p /home/logs/
+	chmod -R 777 /home/logs/
 fi
 if [ ! -d "/home/www/public/" ]; then
 	mkdir -p /home/www/public/
+	chmod -R 777 /home/www/
 fi
 
 # RUN Crontab
@@ -16,10 +19,6 @@ if [ -f "/home/conf/crontab.conf" ]; then
 	crontab /home/conf/crontab.conf
 	cron -f &
 fi
-
-chmod -R 777 /home/conf/
-chmod -R 777 /home/logs/
-chmod -R 777 /home/www/public/
 
 #RUN Apache
 /usr/sbin/apache2 -D FOREGROUND
